f463992a548407ed3192aedb93089abf1ab3cebc
HeeWon <hee.hortus@gmail.com>
2025-08-11 10:15:22 +0900

    feat: Vite & React initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+/* 리액트, 리액트dom 실행 */
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
