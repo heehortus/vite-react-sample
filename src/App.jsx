0a9ad57f3fe993208a3e19abeac41fb7554f6653
HeeWon <hee.hortus@gmail.com>
2025-08-11 13:20:44 +0900

    feat: 홈 페이지 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './css/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <> 
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React 연습</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 /* 리액트, 리액트dom 실행 */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './css/font.css'
+import './css/index.css'
+import Home from './Home/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Home />
   </StrictMode>,
 )
